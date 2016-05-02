4f0ab26ebf0bbfd206cb019729b5a61efb803911
Martin Holly <martin.holly.33@gmail.com>
2016-05-01 23:05:50 -0400

    Added additional tests
    - Added tests for IsSquare property
    - Added some tests for the + operator

---
+++ BakeNeko.Core.Tests/Types/MatrixAdditionTests.cs
@@ -0,0 +1,110 @@
+﻿using BakeNeko.Core.Types;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BakeNeko.Core.Tests.Types
+{
+    [TestClass]
+    public class MatrixAdditionTests
+    {
+        #region PRIVATE FIELDS
+        #endregion
+
+        #region SETUP / TEARDOWN
+        #endregion
+
+        #region TESTS
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Matrix_Addition_NonConformableMatricesCannotBeAdded_Test()
+        {
+            //Setup
+            var mA = new Matrix<Integer>(new Integer[3, 6]);
+            var mB = new Matrix<Integer>(new Integer[4, 5]);
+
+            //Act
+            var mc = mA + mB;
+        }
+
+        [TestMethod]
+        public void Matrix_Addition_OneByOneMatrixAddition_Test()
+        {
+            //Setup
+            var mA = new Matrix<Integer>(new Integer[,] { { 1 } });
+            var mB = new Matrix<Integer>(new Integer[,] { { 2 } });
+
+            var matrixExpected = new Matrix<Integer>(new Integer[,] { { 3 } });
+
+            //Act
+            var mC = mA + mB;
+
+            //Assert
+            Assert.AreEqual(matrixExpected, mC);
+        }
+
+        [TestMethod]
+        public void Matrix_Addition_HorizontalMatrixAddition_Test()
+        {
+            //Setup
+            var mA = new Matrix<Integer>(new Integer[3, 1] { { 1 }, { 2 }, { -3 } });
+            var mB = new Matrix<Integer>(new Integer[3, 1] { { 4 }, { 7 }, { -8 } });
+
+            var matrixExpected = new Matrix<Integer>(new Integer[3, 1] { { 5 }, { 9 }, { -11 } });
+
+            //Act
+            var mC = mA + mB;
+
+            //Assert
+            Assert.AreEqual(matrixExpected, mC);
+        }
+
+        [TestMethod]
+        public void Matrix_Addition_VerticalMatrixAddition_Test()
+        {
+            //Setup
+            var mA = new Matrix<Integer>(new Integer[1, 3] { { 1, 2, 3 } });
+            var mB = new Matrix<Integer>(new Integer[1, 3] { { -34, -5, 6 } });
+
+            var matrixExpected = new Matrix<Integer>(new Integer[1, 3] { { -33, -3, 9 } });
+
+            //Act
+            var mC = mA + mB;
+
+            //Assert
+            Assert.AreEqual(matrixExpected, mC);
+        }
+
+        [TestMethod]
+        public void Matrix_Addition_SquareMatrixAddition_Test()
+        {
+            //Setup
+            var mA = new Matrix<Integer>(new Integer[,] { { 1, 2 }, { -3, 4 } });
+            var mB = new Matrix<Integer>(new Integer[,] { { 7, -7 }, { 8, 8 } });
+
+            var matrixExpected = new Matrix<Integer>(new Integer[,] { { 8, -5 }, { 5, 12 } });
+
+            //Act
+            var mC = mA + mB;
+
+            //Assert
+            Assert.AreEqual(matrixExpected, mC);
+        }
+
+        [TestMethod]
+        public void Matrix_Addition_RectangularMatrixAddition_Test()
+        {
+            //Setup
+            var mA = new Matrix<Integer>(new Integer[,] { { 1, 2, -100 }, { -3, 4, -23 } });
+            var mB = new Matrix<Integer>(new Integer[,] { { 7, -7, 54 }, { 8, 8, 8 } });
+
+            var matrixExpected = new Matrix<Integer>(new Integer[,] { { 8, -5, -46 }, { 5, 12, -15 } });
+
+            //Act
+            var mC = mA + mB;
+
+            //Assert
+            Assert.AreEqual(matrixExpected, mC);
+        }
+        #endregion
+    }
+}
---
+++ BakeNeko.Core.Tests/Types/MatrixUtilityPropertyTests.cs
@@ -0,0 +1,132 @@
+﻿using BakeNeko.Core.Types;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BakeNeko.Core.Tests.Types
+{
+    [TestClass]
+    public class MatrixUtilityPropertyTests
+    {
+        #region PRIVATE FIELDS
+        /*  
+            ┌───┐
+            | 1 |
+            └───┘
+        */
+        private static Integer[,] _array1by1;
+
+        /*  
+            ┌───┬───┐────┐
+            | 1 | 2 | 33 |
+            ├───┼───┤────┤
+            | 4 | 5 | 66 |
+            ├───┼───┤────┤
+            | 7 | 8 | 99 |
+            └───┴───┘────┘
+        */
+        private static Integer[,] _array3by3;
+
+        /*  
+            ┌───┬───┐───┐────┐
+            | 1 | 2 | 3 | 33 |
+            ├───┼───┤───┤────┤
+            | 4 | 5 | 6 | 66 |
+            ├───┼───┤───┤────┤
+            | 7 | 8 | 9 | 99 |
+            └───┴───┘───┘────┘
+        */
+        private static Integer[,] _array4by3;
+
+        /*  
+            ┌────┬────┐────┐
+            | 1  | 2  | 3  |
+            ├────┼────┤────┤
+            | 4  | 5  | 6  |
+            ├────┼────┤────┤
+            | 7  | 8  | 9  |
+            ├────┼────┤────┤
+            | 77 | 88 | 99 |
+            └────┴────┘────┘
+        */
+        private static Integer[,] _array3by4;
+        #endregion
+
+        #region SETUP / TEARDOWN
+        [ClassInitialize]
+        public static void SetUp(TestContext context)
+        {
+            _array1by1 = new Integer[1, 1] { { 1 } };
+            _array3by3 = new Integer[3, 3]
+            {
+                {1, 4, 7},
+                {2, 5, 8 },
+                {33, 66, 99 }
+            };
+            _array4by3 = new Integer[4, 3]
+            {
+                {1, 4, 7 },
+                {2, 5, 8 },
+                {3, 6, 9 },
+                {33, 66, 99 }
+            };
+
+            _array3by4 = new Integer[3, 4]
+            {
+                {1, 4, 7, 77 },
+                {2, 5, 8, 88 },
+                {3, 6, 9, 99 }
+            };
+        }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            _array1by1 = null;
+            _array3by3 = null;
+            _array4by3 = null;
+            _array3by4 = null;
+        }
+        #endregion
+
+        #region TESTS
+        [TestMethod]
+        public void Matrix_IsSquare_DegenerateOnebyOne_Test()
+        {
+            //Arrange
+            var matrix = new Matrix<Integer>(_array1by1);
+
+            //Assert
+            Assert.IsTrue(matrix.IsSquare);
+        }
+
+        [TestMethod]
+        public void Matrix_IsSquare_NonTrivial3by3_Test()
+        {
+            //Arrange
+            var matrix = new Matrix<Integer>(_array3by3);
+
+            //Assert
+            Assert.IsTrue(matrix.IsSquare);
+        }
+
+        [TestMethod]
+        public void Matrix_IsSquare_WidthIsGreaterThenHeight_Test()
+        {
+            //Arrange
+            var matrix = new Matrix<Integer>(_array4by3);
+
+            //Assert
+            Assert.IsFalse(matrix.IsSquare);
+        }
+
+        [TestMethod]
+        public void Matrix_IsSquare_HeightIsGreaterThenWidth_Test()
+        {
+            //Arrange
+            var matrix = new Matrix<Integer>(_array3by4);
+
+            //Assert
+            Assert.IsFalse(matrix.IsSquare);
+        }
+        #endregion
+    }
+}
