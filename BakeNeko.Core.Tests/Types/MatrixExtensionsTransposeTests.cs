f4e9e9de95de862f6423b014ebc01f514246622e
Martin Holly <martin.holly.33@gmail.com>
2016-05-04 23:06:45 -0400

    Addded additional Transpose() extension tests
    - Added a trivial unit matrix test
    - Row & Column matrix tests
    - Non-trivial matrix transpose tests

--- BakeNeko.Core.Tests/Types/MatrixExtensionsTransposeTests.cs
+++ BakeNeko.Core.Tests/Types/MatrixExtensionsTransposeTests.cs
@@ -8,6 +8,19 @@ namespace BakeNeko.Core.Tests.Types
     {
         #region TESTS
         [TestMethod]
+        public void MatrixExtensions_Transpose_UnitMatrixTranspose_Test()
+        {
+            //Arrange
+            var mUnit = new Matrix<Integer>(new Integer[1, 1] { { 33 } });
+
+            //Act
+            var mActual = mUnit.Transpose();
+
+            //Assert
+            Assert.AreEqual(mUnit, mActual);
+        }
+
+        [TestMethod]
         public void MatrixExtensions_Transpose_IdentityTransposedIsIdentity_Test()
         {
             //Arrange
@@ -19,6 +32,80 @@ namespace BakeNeko.Core.Tests.Types
 
             //Assert
             Assert.AreEqual(mIdentityExpected, mActual);
+        }
+
+        [TestMethod]
+        public void MatrixExtensions_Transpose_ColumnMatrixBecomesRowMatrix_Test()
+        {
+            //Arrange
+            var mColumn = new Matrix<Integer>(new Integer[3, 1] { { 1 }, { 2 }, { 3 } });
+            var mExpected = new Matrix<Integer>(new Integer[1, 3] { { 1, 2, 3 } });
+
+            //Act
+            var mActual = mColumn.Transpose();
+
+            //Assert
+            Assert.AreEqual(mExpected, mActual);
+        }
+
+        [TestMethod]
+        public void MatrixExtensions_Transpose_RowMatrixBecomesColumnMatrix_Test()
+        {
+            //Arrange
+            var mRow = new Matrix<Integer>(new Integer[1, 3] { { 1, 2, 3 } });
+            var mExpected = new Matrix<Integer>(new Integer[3, 1] { { 1 }, { 2 }, { 3 } });
+
+            //Act
+            var mActual = mRow.Transpose();
+
+            //Assert
+            Assert.AreEqual(mExpected, mActual);
+        }
+
+        [TestMethod]
+        public void MatrixExtensions_Transpose_NonTrivialTranspose_Test()
+        {
+            //Arrange
+            /*  
+                ┌───┬───┬───┐
+                | 1 | 7 | 0 |
+                ├───┼───┼───┤
+                | 0 | 9 | 0 |
+                ├───┼───┼───┤
+                | 9 | 0 | 4 |
+                ├───┼───┼───┤
+                | 6 | 1 | 8 |
+                └───┴───┴───┘
+            */
+            var m = new Matrix<Integer>(new Integer[3, 4]
+            {
+                {1, 0, 9, 6 },
+                {7, 9, 0, 1 },
+                {0, 0, 4, 8 }
+            });
+
+            /*  
+                ┌───┬───┬───┬───┐
+                | 1 | 0 | 9 | 6 |
+                ├───┼───┼───┼───┤
+                | 7 | 9 | 0 | 1 |
+                ├───┼───┼───┼───┤
+                | 0 | 0 | 4 | 8 |
+                └───┴───┴───┴───┘
+            */
+            var mExpected = new Matrix<Integer>(new Integer[4, 3]
+            {
+                {1, 7, 0 },
+                {0, 9, 0 },
+                {9, 0, 4 },
+                {6, 1, 8 }
+            });
+
+            //Act
+            var mActual = m.Transpose();
+
+            //Assert
+            Assert.AreEqual(mExpected, mActual);
         }
         #endregion
     }
